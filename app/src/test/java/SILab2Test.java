c9c8ee7bbe3e83e8d29782814a9d1661838c688b
Klimentina Efremova <klimentina08642@gmail.com>
2025-05-18 09:54:10 +0200

    Final commit

--- app/src/test/java/SILab2Test.java
+++ app/src/test/java/SILab2Test.java
@@ -11,53 +11,53 @@ public class SILab2Test {
 
     @Test(expected = RuntimeException.class)
     public void test_itemWithNullName_throwsException() {
-        List<Item> items = Arrays.asList(new Item("", 1, 111, 0.0));
-        SILab2.checkCart(items, "1234567890123456");
+        List<Item> items = Arrays.asList(new Item(null, 3, 270, 0.0));
+        SILab2.checkCart(items, "0123456789123456");
     }
 
     @Test(expected = RuntimeException.class)
     public void test_itemWithEmptyName_throwsException() {
-        List<Item> items = Arrays.asList(new Item(null, 3, 70, 0.1));
+        List<Item> items = Arrays.asList(new Item("", 1, 111, 0.2));
         SILab2.checkCart(items, "1234567890123456");
     }
 
     @Test(expected = RuntimeException.class)
     public void test_invalidCardNumber_short() {
-        List<Item> items = Arrays.asList(new Item("Domati", 4, 45, 0.0));
-        SILab2.checkCart(items, "1232344398");
+        List<Item> items = Arrays.asList(new Item("Sladoled", 3, 100, -0.5));
+        SILab2.checkCart(items, "123456789");
     }
 
     @Test(expected = RuntimeException.class)
     public void test_invalidCardNumber_nonDigit() {
-        List<Item> items = Arrays.asList(new Item("Zejtin", 10, 90, -0.3));
+        List<Item> items = Arrays.asList(new Item("Kompiri", 10, 75, 0.1));
         SILab2.checkCart(items, "12345678901234AB");
     }
 
     @Test
     public void test_itemWithDiscount() {
-        List<Item> items = Arrays.asList(new Item("Sladoled", 2, 200, 0.1)); 
-        double expected = -30 + 2 * 200 * 0.9; 
-        double actual = SILab2.checkCart(items, "1234567890123456");
+        List<Item> items = Arrays.asList(new Item("Spageti", 3, 200, 0.1));
+        double expected = -30 + 3 * 200 * 0.9;
+        double actual = SILab2.checkCart(items, "0123456789101112");
         assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void test_itemWithHighPriceNoDiscount() {
-        List<Item> items = Arrays.asList(new Item("Ajvar", 1, 400, 0.0));
-        double expected = -30 + 400; 
-        double actual = SILab2.checkCart(items, "0123456789123456");
+        List<Item> items = Arrays.asList(new Item("Parfem", 1, 650, 0.0));
+        double expected = -30 + 650;
+        double actual = SILab2.checkCart(items, "6543210987654321");
         assertEquals(expected, actual, 0.01);
     }
 
     @Test
     public void test_multipleItems_mixed() {
         List<Item> items = Arrays.asList(
-                new Item("Kromid", 12, 50, 0),         // -30 + 12 * 50 = 570
-                new Item("Sok", 3, 350, 0.2),   //  -30 + 350*0.8*3 = 810
-                new Item("Leb", 20, 20, 0)           // -30 + 20 * 20 = 370
+                new Item("Kromid", 12, 5, 0.0),         // -30 + 12 * 5 = 30
+                new Item("Ajvar", 1, 500, 0.0),   // -30 + 500 = 470
+                new Item("Lebcinja",8 , 5, 0.2)           //  -30 + 8 * 5 * 0.8 = 2
         );
-        double expected = -90 + 600 + 840 + 400;
-        double actual = SILab2.checkCart(items, "01234567890123456");
+        double expected = -90 + 60 + 500 + 32;
+        double actual = SILab2.checkCart(items, "1615141312111098");
         assertEquals(expected, actual, 0.01);
     }
 }
